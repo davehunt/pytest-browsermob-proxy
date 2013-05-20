0828149bbdb339b0e58047f4f8e4d6995b2f5a4c
Dave Hunt <dave.hunt@gmail.com>
2013-05-20 17:03:03 +0100

    Added logging and improved error catching when using basic authentication

--- pytest_browsermob_proxy.py
+++ pytest_browsermob_proxy.py
@@ -5,6 +5,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import json
+import logging
 import os
 
 from browsermobproxy import Server
@@ -60,8 +61,22 @@ def pytest_addoption(parser):
                      help='password for browsermob proxy automatic basic authentication.')
 
 
+def pytest_configure():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+
 @pytest.mark.tryfirst
 def pytest_sessionstart(session):
+    logger = logging.getLogger(__name__)
+
     if hasattr(session.config, 'slaveinput') or session.config.option.collectonly:
         return
 
@@ -71,30 +86,32 @@ def pytest_sessionstart(session):
                 session.config.option.bmp_path,
                 {'port': int(session.config.option.bmp_port)})
             session.config.browsermob_server.start()
+            logger.info('BrowserMob proxy server started (%s:%s)' % (session.config.option.bmp_host, session.config.option.bmp_port))
         else:
             raise Exception('Unable to locate BrowserMob proxy at %s' % session.config.option.browsermob_proxy_path)
 
     session.config.browsermob_session_proxy = session.config.browsermob_server.create_proxy()
-    #print 'BrowserMob session proxy started (%s:%s)' % (session.config.option.bmp_host, session.config.browsermob_session_proxy.port)
+    logger.info('BrowserMob session proxy started (%s:%s)' % (session.config.option.bmp_host, session.config.browsermob_session_proxy.port))
     configure_browsermob_proxy(session.config.browsermob_session_proxy, session.config)
 
 
 @pytest.mark.tryfirst
 def pytest_runtest_setup(item):
+    logger = logging.getLogger(__name__)
+
     if item.config.option.bmp_test_proxy and \
             hasattr(item.config, 'browsermob_server') and \
             'skip_browsermob_proxy' not in item.keywords:
 
         item.config.browsermob_test_proxy = item.config.browsermob_server.create_proxy()
-        #print 'BrowserMob test proxy started (%s:%s)' % (item.config.option.bmp_host, item.config.browsermob_test_proxy.port)
+        logger.info('BrowserMob test proxy started (%s:%s)' % (item.config.option.bmp_host, item.config.browsermob_test_proxy.port))
         configure_browsermob_proxy(item.config.browsermob_test_proxy, item.config)
         #TODO make recording of har configurable
         item.config.browsermob_test_proxy.new_har()
 
 
 def pytest_runtest_teardown(item):
-    if hasattr(item.config, 'browsermob_test_proxy'):
-        item.config.browsermob_test_proxy.close()
+    stop_test_proxy(item.config)
 
 
 def pytest_runtest_makereport(__multicall__, item, call):
@@ -108,22 +125,67 @@ def pytest_runtest_makereport(__multicall__, item, call):
 def pytest_sessionfinish(session):
     if hasattr(session.config, 'slaveinput') or session.config.option.collectonly:
         return
-
-    if hasattr(session.config, 'browsermob_session_proxy'):
-        session.config.browsermob_session_proxy.close()
-
-    if hasattr(session.config, 'browsermob_server'):
-        session.config.browsermob_server.stop()
+    stop_all(session.config)
 
 
 def configure_browsermob_proxy(proxy, config):
+    logger = logging.getLogger(__name__)
+
     if config.option.bmp_headers:
         proxy.headers(json.loads(config.option.bmp_headers))
 
-    if all([config.option.bmp_domain,
-            config.option.bmp_username,
-            config.option.bmp_password]):
-        proxy.basic_authentication(
-            config.option.bmp_domain,
-            config.option.bmp_username,
-            config.option.bmp_password)
+    basic_authentication_config = [
+        config.option.bmp_domain,
+        config.option.bmp_username,
+        config.option.bmp_password]
+    if any(basic_authentication_config):
+        if not all(basic_authentication_config):
+            logger.error('Unable to configure basic authentication. Missing requried value(s)')
+            stop_all(config)
+            raise AuthenticationConfigurationException(*basic_authentication_config)
+        else:
+            proxy.basic_authentication(*basic_authentication_config)
+
+
+def stop_all(config):
+    stop_test_proxy(config)
+    stop_session_proxy(config)
+    stop_server(config)
+
+
+def stop_session_proxy(config):
+    logger = logging.getLogger(__name__)
+
+    if hasattr(config, 'browsermob_session_proxy'):
+        config.browsermob_session_proxy.close()
+        logger.info('BrowserMob session proxy stopped (%s:%s)' % (config.option.bmp_host, config.browsermob_session_proxy.port))
+
+
+def stop_test_proxy(config):
+    logger = logging.getLogger(__name__)
+
+    if hasattr(config, 'browsermob_test_proxy'):
+        config.browsermob_test_proxy.close()
+        logger.info('BrowserMob test proxy stopped (%s:%s)' % (config.option.bmp_host, config.browsermob_test_proxy.port))
+
+
+def stop_server(config):
+    logger = logging.getLogger(__name__)
+
+    if hasattr(config, 'browsermob_server'):
+        config.browsermob_server.stop()
+        logger.info('BrowserMob proxy server stopped (%s:%s)' % (config.option.bmp_host, config.option.bmp_port))
+
+
+class AuthenticationConfigurationException(Exception):
+
+    def __init__(self, domain, username, password):
+        missing_values = []
+        config = {'domain': domain,
+                  'username': username,
+                  'password': password}
+        for key, value in config.iteritems():
+            if value is None:
+                missing_values.append(key)
+        message = 'Unable to configure basic authentication. Missing required value(s): %s' % ', '.join(missing_values)
+        Exception.__init__(self, message)
